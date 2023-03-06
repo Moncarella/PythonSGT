6ae4e2f31a73b032334a0d700eca7b6368830149
moncarella <ramoni612@gmail.com>
2023-03-06 19:03:44 +0200

    Adding the changes

--- 2nd week/2_practical_Artūrs example.py
+++ 2nd week/2_practical_Artūrs example.py
@@ -1,1 +1,11 @@
 ### Only adding things, that were done wrong or differently ###
+
+#######   Task 1   ########
+age = int(input("Enter your age:"))
+hasDrivingLicence = input("Do you have a driving licence (y/n)?")
+
+result = age >= 18 and hasDrivingLicence == "y"
+print("You are able to drive: " + str(result))
+
+
+#######   Task 2   ########
