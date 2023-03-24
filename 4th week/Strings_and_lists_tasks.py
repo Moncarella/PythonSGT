524d2c041a834933fa8bc1558e8dc7ec21d1f54a
moncarella <ramoni612@gmail.com>
2023-03-24 20:15:02 +0200

    Uploading 4 week progress

--- 4th week/Strings_and_lists_tasks.py
+++ 4th week/Strings_and_lists_tasks.py
@@ -1,39 +1,39 @@
 # Task 1
 # Ask the user to enter the text and a letter. Count how many occurrences of the letter provided
-# import re
+import re
 
-# text = input("Please enter the text:\n")
-# letter = input("Please enter a letter to find in the text:\n")
-# match = len(re.findall(letter, text))
+text = input("Please enter the text:\n")
+letter = input("Please enter a letter to find in the text:\n")
+match = len(re.findall(letter, text))
 
-# if match:
-#     print("There are", match, "occurencies of", letter, "in the provided text")
-# else:
-#     print("Your specified letter is not in the text")
+if match:
+    print("There are", match, "occurencies of", letter, "in the provided text")
+else:
+    print("Your specified letter is not in the text")
 
 
 # Task 1.1
 # Based on the task 1, count the occurrences of each character in the text provided and display them in the output
 
-# text = input("Please enter the text:\n")
-# letters = []
-# letterCount = []
+text = input("Please enter the text:\n")
+letters = []
+letterCount = []
 
-# for i in range(0, text.__len__()):
-#     letterExist = False
-#     if text[i] == " ":
-#         continue
-#     for j in range(0, letters.__len__()):
-#         if letters[j] == text[i]:
-#             letterCount[j] += 1
-#             letterExist = True
-#             break
-#     if not letterExist:
-#         letters.append(text[i])
-#         letterCount.append(1)
+for i in range(0, text.__len__()):
+    letterExist = False
+    if text[i] == " ":
+        continue
+    for j in range(0, letters.__len__()):
+        if letters[j] == text[i]:
+            letterCount[j] += 1
+            letterExist = True
+            break
+    if not letterExist:
+        letters.append(text[i])
+        letterCount.append(1)
 
-# for i in range(0, letters.__len__()):
-#     print(letters[i] + " : " + str(letterCount[i]))
+for i in range(0, letters.__len__()):
+    print(letters[i] + " : " + str(letterCount[i]))
 
 
 # Task 2
