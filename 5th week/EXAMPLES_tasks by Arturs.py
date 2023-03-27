dee83c9422d9e0cd5f4b95d33d87092d15c29b53
moncarella <ramoni612@gmail.com>
2023-03-27 18:47:23 +0300

    Uploading tasks example

---
+++ 5th week/EXAMPLES_tasks by Arturs.py
@@ -0,0 +1,40 @@
+# # Task 1
+# def sum(a,b):
+#     return a + b
+
+# a = 12
+# b = 19
+# print(sum(a,b))
+
+# # Task 2
+# def count_vowels(text):
+#     count = 0
+#     for char in text:
+#         if char in "aeiouAEIOU":
+#             count += 1
+#     return count
+
+# textInp = input("Enter the text\n")
+# print("There are " + str(count_vowels(textInp)) + " vowels in the text")
+
+# Task 3
+# def reverse(text:str):
+#     revStr = ""
+#     for char in text:
+#         revStr = char + revStr
+#     return revStr
+
+# def is_palindrome(text:str):
+#     text = text.lower()
+#     if reverse(text) == text:
+#         return True
+#     else:
+#         return False
+
+# textInput = input("Enter the text:\n")
+# print(is_palindrome(textInput))
+
+# Task 4
+
+
+
